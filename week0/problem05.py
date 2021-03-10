3f012b524aa7ef5972f6e0620f511f7bc3a34e97
minh26799 <62030140+minh26799@users.noreply.github.com>
2021-03-10 21:46:05 +0700

    Week0 assignment

--- week0/problem05.py
+++ week0/problem05.py
@@ -2,4 +2,4 @@ THICKNESS = 0.00008
 folded_thickness_array = [THICKNESS]
 for i in range(0,43):
     folded_thickness_array.append(folded_thickness_array[i]*2)
-print("Thichness: {} meters".format(folded_thickness_array[43]))
+print("Thichness: {} meters.\nThe Length of array: {} elements".format(folded_thickness_array[43], len(folded_thickness_array)))
